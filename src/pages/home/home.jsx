56a3710496f3ed1ff005b1feba7cd02be99c78a1
Khavin Shankar <khavinshankar@gmail.com>
2020-04-24 06:39:45 +0530

    homepage along with related components, directory, and menu-item added

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./home.scss";
+
+import Directory from "../../components/directory/directory";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
