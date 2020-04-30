5b62a6d56c0144f668620c3dbaa8a1ba9f13e863
Khavin Shankar <khavinshankar@gmail.com>
2020-04-30 20:32:21 +0530

    for performance, memoized cart-item component

--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 import {
   CartItemContainer,
@@ -20,4 +20,4 @@ const CartItem = ({ item: { name, price, imageUrl, quantity } }) => {
   );
 };
 
-export default CartItem;
+export default memo(CartItem);
